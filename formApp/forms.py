1c8b1b270bbb78e56b6631e586b890e2f38bc32c
krpraveen0 <pkec151015@gmail.com>
2021-12-13 09:20:47 +0530

    task feedback form view

--- formApp/forms.py
+++ formApp/forms.py
@@ -1,6 +1,36 @@
 from django import forms
+from django.core import validators
+
 
 class StudentForm(forms.Form):
     name = forms.CharField(max_length=100)
     marks = forms.IntegerField()
-    
+
+class FeedBackForm(forms.Form):
+    name=forms.CharField()
+    rollno=forms.IntegerField()
+    email=forms.EmailField()
+    feedback=forms.CharField(widget=forms.Textarea)
+
+    #defining the clean method
+    def clean_name(self):
+        inputName = self.cleaned_data['name']
+        if len(inputName) <4:
+            raise forms.ValidationError('The minimum number of characters in the name field should be 4')
+        return inputName
+    #The returned value of clean method will be considered  by Django at The
+    #time of submitting the form.
+
+    def clean_rollno(self):
+        inputRollno=self.cleaned_data['rollno']
+        print('Validating the roll no')
+        return inputRollno
+    def clean_email(self):
+        inputEmail = self.cleaned_data['email']
+        print('VAlidating the email')
+        return inputEmail
+    def clean_feedback(self):
+        inputFeedback = self.cleaned_data['feedback']
+        print("Validating feedback field")
+        return inputFeedback
+        
--- formApp/views.py
+++ formApp/views.py
@@ -8,8 +8,10 @@ def student_input_view(request):
         form = StudentForm(request.POST)
         if form.is_valid():
             print("Form Validation Success and printing the data")
-            print("Name:",form.clean_data['name'])
-            print("Marks:",form.clean_data['marks'])
+            print("Name:",form.cleaned_data['name'])
+            print("Marks:",form.cleaned_data['marks'])
             sent = True
     form = StudentForm()
-    return render(request,'formApp/input.html',{'from':form,'sent':sent})
+    return render(request,'formApp/input.html',{'form':form,'sent':sent})
+
+#create a view for the feedback form 
